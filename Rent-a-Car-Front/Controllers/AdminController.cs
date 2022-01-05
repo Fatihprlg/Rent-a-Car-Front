3a7de226c43ee38e412f9710f97fea42fe0d6fd6
beraozdogan <61747031+beraozdogan@users.noreply.github.com>
2022-01-05 19:00:50 +0300

    Users page completed

--- Rent-a-Car-Front/Controllers/AdminController.cs
+++ Rent-a-Car-Front/Controllers/AdminController.cs
@@ -21,10 +21,141 @@ namespace Rent_a_Car_Front.Controllers
         {
             return View();
         }
-        public ActionResult Users()
+         public ActionResult Users()
         {
-            return View();
+            if (Logged)
+            {
+                try
+                {
+                    IList<Employee> employees;
+                    using (var logic = new EmployeeLogic())
+                    {
+                        employees = logic.SelectAllEmployees();
+                    }
+                    return View(employees);
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception(ex.Message);
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
         }
+        public ActionResult DeleteUser(int id)
+        {
+            try
+            {
+                bool isSuccess;
+                using (var logic = new EmployeeLogic())
+                {
+                    isSuccess = logic.DeleteEmployeeById(id);
+                }
+                return RedirectToAction("Users");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult InsertUser()
+        {
+            try
+            {
+                bool isSuccess;
+                using (var logic = new EmployeeLogic())
+                {
+                    Employee temp = new Employee()
+                    {
+                        Isim = Request.Form["isim"],
+                        Soyisim = Request.Form["soyisim"],
+                        Telefon = Request.Form["telefon"],
+                        Email = Request.Form["mail"],
+                        Sifre = Request.Form["sifre"],
+                        TC = Request.Form["tc"]
+                    };
+                    isSuccess = logic.InsertEmployee(temp);
+                }
+                return RedirectToAction("Users");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult SaveUserChanges()
+        {
+            try
+            {
+                bool isSuccess;
+                using (var logic = new EmployeeLogic())
+                {
+                    Employee temp = new Employee()
+                    {
+                        ID = int.Parse(Request.Form["ID"]),
+                        Isim = Request.Form["isim"],
+                        Soyisim = Request.Form["soyisim"],
+                        Telefon = Request.Form["telefon"],
+                        Email = Request.Form["mail"],
+                        Sifre = Request.Form["sifre"],
+                        TC = Request.Form["tc"]
+                    };
+                    isSuccess = logic.UpdateEmployee(temp);
+                }
+                return RedirectToAction("Users");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult FilterUsers()
+        {
+            if (Logged)
+            {
+                try
+                {
+                    var id = Request.Form["id"];
+                    var isim = Request.Form["isim"];
+                    IList<Employee> employees = new List<Employee>();
+                    if (!string.IsNullOrEmpty(id) && !string.IsNullOrWhiteSpace(id))
+                    {
+                        using (var logic = new EmployeeLogic())
+                        {
+                            employees.Add(logic.SelectEmployeeById(int.Parse(id)));
+                        }
+                    }
+                    if(!string.IsNullOrEmpty(isim) && !string.IsNullOrWhiteSpace(isim))
+                    {
+                        using (var logic = new EmployeeLogic())
+                        {
+                            var temp = logic.SelectAllEmployees().Where(x => x.Isim == isim);
+                            foreach (var a in temp)
+                            {
+                                employees.Add(a);
+                            }
+                        }
+                    }
+                    return View("Users",employees);
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception(ex.Message);
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
         public ActionResult EditVehicle()
         {
             return View();
