f401545896394f6091951a90a27e88f83cb1a622
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-01-05 19:02:43 +0300

    Front-Back connection completed

---
+++ Rent-a-Car-Front/Models/RentInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rent_a_Car_Front.Models
+{
+    public class RentInfo
+    {
+        public int RentID { get; set; }
+        public string Picture { get; set; }
+        public string VehiclePlateNum { get; set; }
+        public string VehicleModel { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerFullName { get; set; }
+        public string State { get; set; }
+        public byte LicenceAge { get; set; }
+        public byte CustomerAge { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
