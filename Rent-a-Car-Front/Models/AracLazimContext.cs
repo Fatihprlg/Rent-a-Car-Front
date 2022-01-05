c713adea5a2a370b5793ac0a18430694bc71ba72
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-01-05 19:06:27 +0300

    entity context and filter models

---
+++ Rent-a-Car-Front/Models/AracLazimContext.cs
@@ -0,0 +1,30 @@
+﻿using Rent_a_Car.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace Rent_a_Car_Front.Models
+{
+    public class AracLazimContext : DbContext
+    {
+
+        public AracLazimContext()
+            : base("name=AracLazimContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<Araba> Araba { get; set; }
+        public virtual DbSet<Calisan> Calisan { get; set; }
+        public virtual DbSet<Musteri> Musteri { get; set; }
+        public virtual DbSet<Yonetici> Yonetici { get; set; }
+        public virtual DbSet<KiralamaIslemi> KiralamaIslemi { get; set; }
+    }
+}
---
+++ Rent-a-Car-Front/Models/VehicleFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rent_a_Car_Front.Models
+{
+    public class VehicleFilter
+    {
+        public string PickUpLocation { get; set; }
+        public string DropOffLocation { get; set; }
+        public string PickUpDate { get; set; }
+        public string DropOffDate { get; set; }
+        public string PickUpTime { get; set; }
+    }
+}
