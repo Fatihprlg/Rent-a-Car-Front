10444236858f9bf7b72e381838086b58d79661e9
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-01-05 19:07:45 +0300

    Admin controller completed

--- Rent-a-Car-Front/Controllers/AdminController.cs
+++ Rent-a-Car-Front/Controllers/AdminController.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using Rent_a_Car.Models.Concerets;
+using Rent_a_Car.BusinessLogic.Concerets;
+using Rent_a_Car.Conceretes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using Rent_a_Car_Front.Models;
 
 namespace Rent_a_Car_Front.Controllers
 {
     public class AdminController : Controller
     {
+        public static bool Logged = false;
         // GET: Admin
-                public ActionResult AdminPanel()
+        public ActionResult AdminPanel()
         {
             if (Logged)
             {
@@ -112,9 +118,17 @@ namespace Rent_a_Car_Front.Controllers
         }
         public ActionResult Vehicles()
         {
-            return View();
+            if (Logged)
+            {
+                IList<Vehicle> vehicles = SelectAllVehicles();
+                return View(vehicles);
+            }
+            else
+            {
+                return View("Login");
+            }
         }
-         public ActionResult Users()
+        public ActionResult Users()
         {
             if (Logged)
             {
@@ -249,9 +263,180 @@ namespace Rent_a_Car_Front.Controllers
             }
         }
 
-        public ActionResult EditVehicle()
+        public ActionResult EditVehicle(int ID)
+        {
+            if (Logged)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+        public ActionResult SaveVehicleChanges(int ID)
+        {
+            try
+            {
+                Vehicle vehicle = new Vehicle
+                {
+                    ID = ID,
+                    Resim = Request.Form["resim"].ToString(),
+                    Plaka = Request.Form["plaka"].ToString(),
+                    Marka = Request.Form["marka"].ToString(),
+                    Model = Request.Form["model"].ToString(),
+                    GunlukKMSinir = Convert.ToInt32(Request.Form["gunlukKM"]),
+                    Kilometre = Convert.ToInt32(Request.Form["km"]),
+                    YakitTuru = Request.Form["yakit"],
+                    Aciklama = Request.Form["aciklama"],
+                    KoltukSayisi = Convert.ToByte(Request.Form["koltuk"]),
+                    BagajHacmi = Convert.ToInt32(Request.Form["bagaj"]),
+                    GunlukFiyat = Convert.ToDouble(Request.Form["fiyat"]),
+                    SanzimanTuru = Request.Form["vites"]
+                };
+                using (var logic = new VehicleLogic())
+                {
+                    logic.UpdateVehicle(vehicle);
+                }
+                return RedirectToAction("EditVehicle");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult NewVehicle()
         {
             return View();
         }
+        public ActionResult AddVehicle() 
+        {
+            try
+            {
+                Vehicle vehicle = new Vehicle
+                {
+                    Resim = Request.Form["resim"].ToString(),
+                    Plaka = Request.Form["plaka"].ToString(),
+                    Marka = Request.Form["marka"].ToString(),
+                    Model = Request.Form["model"].ToString(),
+                    GunlukKMSinir = Convert.ToInt32(Request.Form["gunlukKM"]),
+                    Kilometre = Convert.ToInt32(Request.Form["km"]),
+                    YakitTuru = Request.Form["yakit"],
+                    Aciklama = Request.Form["aciklama"],
+                    KoltukSayisi = Convert.ToByte(Request.Form["koltuk"]),
+                    BagajHacmi = Convert.ToInt32(Request.Form["bagaj"]),
+                    GunlukFiyat = Convert.ToDouble(Request.Form["fiyat"]),
+                    SanzimanTuru = Request.Form["vites"]
+                };
+                using (var logic = new VehicleLogic())
+                {
+                    logic.InsertVehicle(vehicle);
+                }
+                return RedirectToAction("Vehicles");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult Login()
+        {
+            try
+            {
+                bool response = false;
+                if (Request.Form["admin-radio"] != null)
+                {
+                    if (Request.Form["admin-radio"].ToString().Equals("admin"))
+                    {
+
+                        using (var logic = new AdminLogic())
+                        {
+                            response = logic.LoginControl(Request.Form["email"].ToString(), Request.Form["pwd"].ToString());
+                        }
+                        Logged = response;
+                    }
+                    else if (Request.Form["admin-radio"].ToString().Equals("employee"))
+                    {
+                        using (var logic = new EmployeeLogic())
+                        {
+                            response = logic.LoginControl(Request.Form["email"].ToString(), Request.Form["pwd"].ToString());
+                        }
+                        Logged = response;
+                    }
+                    if (Logged) return RedirectToAction("AdminPanel");
+                    else return RedirectToAction("Login", "Home");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Giriş tipi belirleyiniz')</script>");
+                    return RedirectToAction("Login", "Home"); 
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public ActionResult FilterVehicles()
+        {
+            try
+            {
+                var vehicles = SelectAllVehicles();
+                
+                string marka = Request.Form["marka"];
+                string plaka = Request.Form["plaka"];
+                if (!string.IsNullOrEmpty(marka) && !string.IsNullOrWhiteSpace(marka))
+                    vehicles = vehicles.Where(v => v.Marka.ToUpper() == marka.ToUpper()).ToList();
+                if (!string.IsNullOrEmpty(plaka) && !string.IsNullOrWhiteSpace(plaka))
+                    vehicles = vehicles.Where(v => v.Plaka.ToUpper() == plaka.ToUpper()).ToList();
+                if ((!string.IsNullOrEmpty(marka) && !string.IsNullOrWhiteSpace(marka)) && (!string.IsNullOrEmpty(plaka) && !string.IsNullOrWhiteSpace(plaka)))
+                    vehicles = vehicles.Where(v => (v.Marka.ToUpper() == marka.ToUpper()) && (v.Plaka.ToUpper() == plaka.ToUpper()) ).ToList();
+
+                return View("Vehicles", vehicles);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        
+        private IList<Vehicle> SelectAllVehicles()
+        {
+            try
+            {
+                IList<Vehicle> response = new List<Vehicle>();
+                using(var logic = new VehicleLogic())
+                {
+                    response = logic.SelectAllVehicles();
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        private Customer SelectCustomerByID(int ID)
+        {
+            try
+            {
+                Customer response = new Customer();
+                using (var logic = new CustomerLogic())
+                {
+                    response = logic.SelectCustomerById(ID);
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
     }
+
 }
